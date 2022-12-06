68e24c566154210ec1f480053a1c934d7ba575f4
Jesse Lisser <jesselisser@gmail.com>
2022-12-06 12:14:37 +0100

    day6

---
+++ aoc/day6/a.py
@@ -0,0 +1,28 @@
+import sys
+
+MARKER_SIZE = 4
+
+
+def main(input_: str) -> None:
+    for i in range(0, len(input_) - (MARKER_SIZE - 1)):
+        chunk = input_[i:i + MARKER_SIZE]
+        if len(set(chunk)) == MARKER_SIZE:
+            print(i + MARKER_SIZE)
+            return
+
+
+def test_main():
+    main("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+    main("bvwbjplbgvbhsrlpgdmjqwftvncz")
+    main("nppdvjthqldpwncqszvftbrmjlhg")
+    main("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+    main("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+
+
+if __name__ == "__main__":
+
+    # # read the input from stdin
+    stdin = sys.stdin.read()
+    main(stdin)
+
+    # test_main()
